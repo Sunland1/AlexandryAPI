65cec138e421c84c36447409a0a2b4ca0ec1d853
nantunes <nicolas.antunes@ubicast.eu>
2021-09-24 10:26:08 +0200

    add all API Rest methods

--- Back/app.py
+++ Back/app.py
@@ -1,32 +1,39 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify, Response
 import model
+import json
 
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@app.route("/book" , methods=["GET","POST"] )
-def bookAll() : 
-    if request.method == "GET" :
-        return model.get_book_all()
+@app.route("/book", methods=["GET", "POST"])
+def book_all():
+    if request.method == "GET":
+        body, status = model.get_book_all()
+        return body
+    elif request.method == 'POST':
+        new_book = request.json
+        r_ok = model.add_book(new_book)
+        return Response(status=r_ok)
 
 
-
-
-@app.route("/book/<int:id_book>" , methods=["GET","PUT","DELETE"] )
-def book(id_book) : 
-    if request.method == "GET" : 
-        return {}
-    elif request.method == "PUT" :
-        data_to_modify = jsonify(request.json)
-        return {}
-    elif request.method == "DELETE" :
-        return {}
-    else : 
+@app.route("/book/<int:id_book>", methods=["GET", "PUT", "DELETE"])
+def book(id_book):
+    if request.method == "GET":
+        book, status = model.get_book(id_book)
+        return book
+    elif request.method == "PUT":
+        data_to_modify = request.json
+        book, status = model.modify_book(data_to_modify, id_book)
+        return book
+    elif request.method == "DELETE":
+        body, status = model.delete_book(id_book)
+        return Response(status=status)
+    else:
         return "test"
-
 
--- Back/model.py
+++ Back/model.py
@@ -1,43 +1,71 @@
 import json
 
-def getStore() : 
-    with open("./store.json" , "r" ) as file :
+
+def get_store():
+    with open("./store.json", "r") as file:
         return json.load(file)
 
 
-def saveStore(new_store) : 
-    with open("./store.json" , "w") as file :
+def save_store(new_store):
+    with open("./store.json", "w") as file:
         json.dump(new_store , file)
 
+
 def add_book(data):
-    try :
-        store = getStore()
-        id_book = len(store) + 1 
+    try:
+        store = get_store()
+        id_book = len(store)
         store[id_book] = data
-        saveStore(store)
+        save_store(store)
         return 200
 
-    except Exception as e : 
+    except Exception as e:
         print(f"failed to add book : {e}")
         return 500
 
-def get_Book(id_book) : 
-    try :
-        store = getStore()
-        return 200 , store[id_book]
-    except Exception as e : 
+
+def modify_book(data, id_book):
+    id_book = str(id_book)
+    try:
+        store = get_store()
+        for key, info in data.items():
+            if key == 'year':
+                info = int(info)
+            store[id_book][key] = info
+        save_store(store)
+        return store[id_book], 200
+    except Exception as e:
+        print(f"failed to modify book : {e}")
+        return {}, 500
+
+
+def delete_book(id_book):
+    id_book = str(id_book)
+    try:
+        store = get_store()
+        del store[id_book]
+        save_store(store)
+        return {}, 200
+    except Exception as e:
+        print(f"failed to modify book : {e}")
+        return {}, 500
+
+
+def get_book(id_book):
+    id_book = str(id_book)
+    try:
+        store = get_store()
+        return store[id_book], 200
+    except Exception as e:
         print(f"failed to get book : {e}")
-        return 500
+        return {}, 500
 
 
-
-def get_book_all() :
-    try :
-        store = getStore()
-        return {
-            "statusCode" : 200 ,
-            "body" : store
-        }
-    except Exception as e : 
+def get_book_all():
+    try:
+        store = get_store()
+        return store, 200
+    except Exception as e:
         print(f"failed to get books : {e}")
-        return 500
+        return {}, 500
+
